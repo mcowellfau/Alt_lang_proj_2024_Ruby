a4ba6d05c257f2efaeab037cddd8cbc7dec4c61e
mcowellfau <mcowell2020@fau.edu>
2024-04-05 22:51:58 -0400

    put require statement at the top of the file and renamed attributed

--- main.rb
+++ main.rb
@@ -1,28 +1,29 @@
+require 'csv'
 class Cell
-    attr_accessor :oem, :model, :launch_announced, :launch_status,
-                  :body_dimensions, :body_weight, :body_sim, :display_type,
-                  :display_size, :display_resolution, :features_sensors, :platform_os
+    attr_accessor :oem, :model, :launchAnnounced, :launchStatus,
+                  :bodyDimensions, :bodyWeight, :bodySim, :displayType,
+                  :displaySize, :displayResolution, :featuresSensors, :platformOs
   
-    def initialize(oem, model, launch_announced, launch_status,
-                   body_dimensions, body_weight, body_sim, display_type,
-                   display_size, display_resolution, features_sensors, platform_os)
+    def initialize(oem, model, launchAnnounced, launchStatus,
+                   bodyDimensions, bodyWeight, bodySim, displayType,
+                   displaySize, displayResolution, featuresSensors, platformOs)
       @oem = oem
       @model = model
-      @launch_announced = launch_announced
-      @launch_status = launch_status
-      @body_dimensions = body_dimensions
-      @body_weight = body_weight
-      @body_sim = body_sim
-      @display_type = display_type
-      @display_size = display_size
-      @display_resolution = display_resolution
-      @features_sensors = features_sensors
-      @platform_os = platform_os
+      @launchAnnounced = launchAnnounced
+      @launchStatus = launchStatus
+      @bodyDimensions = bodyDimensions
+      @bodyWeight = bodyWeight
+      @bodySim = bodySim
+      @displayType = displayType
+      @displaySize = displaySize
+      @displayResolution = displayResolution
+      @featuresSensors = featuresSensors
+      @platformOs = platformOs
     end
   end
   
   
-  require 'csv'
+  
   
   def clean_data(value, column_name)
     
@@ -30,9 +31,9 @@ class Cell
   
   csv_data = CSV.read('cells.csv', headers: true).map do |row|
     Cell.new(
-      row['oem'], row['model'], row['launch_announced'].to_i, row['launch_status'],
-      row['body_dimensions'], row['body_weight'].to_f, row['body_sim'], row['display_type'],
-      row['display_size'], row['display_resolution'], row['features_sensors'], row['platform_os']
+      row['oem'], row['model'], row['launchAnnounced'].to_i, row['launchStatus'],
+      row['bodyDimensions'], row['bodyWeight'].to_f, row['bodySim'], row['displayType'],
+      row['displaySize'], row['displayResolution'], row['featuresSensors'], row['platformOs']
     )
   end
   
